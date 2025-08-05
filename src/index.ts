163e6190429f87a8678c7a3880952cac8c3a5ef9
Emanuele DelBono <emanuele@codiceplastico.com>
2025-08-05 12:27:13 +0200

    with hrm plugin

--- src/index.ts
+++ src/index.ts
@@ -16,63 +16,49 @@ import {
   withModifier,
   hyperLayer,
 } from 'karabiner.ts'
+import { hrm } from "karabiner.ts-greg-mods";
 
 // ! Change '--dry-run' to your Karabiner-Elements Profile name.
 // (--dry-run print the config json into console)
 // + Create a new profile if needed.
 writeToProfile('Default', [
+  rule("Home row mods").manipulators(
+     hrm(
+       new Map([
+         ["a", "l⇧"],
+         ["s", "l⌃"],
+         ["d", "l⌥"],
+         ["f", "l⌘"],
 
-  duoLayer('d', 'f').manipulators([
-    map('1').to('f1'),
-    map('2').to('f2'),
-    map('3').to('f3'),
-    map('4').to('f4'),
-    map('5').to('f5'),
-    map('6').to('f6'),
-    map('7').to('f7'),
-    map('8').to('f8'),
-    map('9').to('f9'),
-    map('0').to('f10'),
-    map('-').to('f11'),
-    map('=').to('f12'),
-  ]),
-
-  duoLayer('j', 'k').manipulators([
-    map('1').to('f1'),
-    map('2').to('f2'),
-    map('3').to('f3'),
-    map('4').to('f4'),
-    map('5').to('f5'),
-    map('6').to('f6'),
-    map('7').to('f7'),
-    map('8').to('f8'),
-    map('9').to('f9'),
-    map('0').to('f10'),
-    map('-').to('f11'),
-    map('=').to('f12'),
-  ]),
-
+         ["j", "l⌘"],
+         ["k", "l⌥"],
+         ["l", "l⌃"],
+         [";", "l⇧"],
+      ])
+     )
+     .lazy(true)
+     .holdTapStrategy("permissive-hold")
+     .chordalHold(true)
+     .build()
+   ),
   layer('caps_lock', 'caps_lock pressed')
-    .configKey((v) => v.toIfAlone('escape'), true)
-    .modifiers('??')
-    .manipulators({
-      "return_or_enter": toKey('delete_or_backspace'),
-      a: toKey('left_shift'),
-      s: toKey('left_control'),
-      d: toKey('left_option'),
-      f: toKey('left_command'),
-      g: toKey('tab'),
-      h: toKey('tab', 'left_shift'),
-      i: toIfAlone('up_arrow'),
-      j: toKey('left_arrow'),
-      k: toKey('down_arrow'),
-      l: toKey('right_arrow'),
-    }),
-  rule('accent').manipulators([
-    map('.', 'right_command').to('grave_accent_and_tilde', 'left_option'),
-    map('e', 'right_command').to('e', 'left_option'),
-  ]),
+      .configKey((v) => v.toIfAlone('escape'), true)
+      .modifiers('??')
+      .manipulators({
+        return_or_enter: toKey('delete_or_backspace'),
+        a: toKey('left_shift'),
+        s: toKey('left_control'),
+        d: toKey('left_option'),
+        f: toKey('left_command'),
+        i: toKey('up_arrow'),
+        j: toKey('left_arrow'),
+        k: toKey('down_arrow'),
+        l: toKey('right_arrow'),
+      })
+
 ])
+
+
 
 /*
 Karabiner-Elements profile parameters can also be set by the 3rd parameter
